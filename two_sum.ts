73faf8773444a32afab454cda279db29f625617c
harcop <harcopdrew@gmail.com>
2020-08-18 23:55:47 +0100

    two sum while loop

--- two_sum.ts
+++ two_sum.ts
@@ -27,3 +27,26 @@ function two_sum(arr, value) {
 //         console.log(hash[ele]);
 //     };
 // }
+
+//we need to sort the array first;
+// function two_sum(arr, value) {
+//     arr.sort((a, b) => a - b);
+//     console.log(arr);
+//     let left = 0;
+//     let right = arr.length - 1;
+//     while(left < right) {
+//         let currentSum = arr[left] + arr[right];
+//         console.log(currentSum);
+//         if(currentSum < value) {
+//             left += 1;
+//         }
+//         else if (currentSum > value) {
+//             right -= 1;
+//         }
+//         else if (currentSum === value) {
+//             return [arr[left], arr[right]];
+//         }
+//     }
+//     return []
+// }
+
