09134baa422438c3bc6e7a682013da7f33f17a06
harcop <harcopdrew@gmail.com>
2020-08-31 04:24:30 +0100

    house of cat

---
+++ algo/house_of_cat.ts
@@ -0,0 +1,23 @@
+function houseOfCats(l: number): number[] {
+    let _a = [2,4];
+    let _l = Array(l+1);
+    _l.fill(0,0,l+1);
+    _l[0] = 1;
+    _a.forEach(ele => {
+        if (ele > l) return;
+        for(let i=ele; i<_l.length; i = i+2) {
+            let _d = i - ele;
+            _l[i] += _l[_d];
+        }
+    });
+    let _na = [];
+    let k = 1;
+    for(let i = 1; i<=_l[l]; i++) {
+        _na.push(k);
+        k += 2;
+    };
+    return _na;
+}
+
+console.log(houseOfCats(10));
+console.log(houseOfCats(2));
