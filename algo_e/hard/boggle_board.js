e95fb5b2b1c9424b025dc60e8f6414247bc1241c
harcop <harcopdrew@gmail.com>
2020-10-26 22:33:02 +0100

    feat(): boggle board update

--- algo_e/hard/boggle_board.js
+++ algo_e/hard/boggle_board.js
@@ -12,19 +12,7 @@ const arr = [
 const w = arr[0].length-1;
 const h = arr.length-1;
 let vt = [];
-
-function rs (_n, c) {
-    let k = Object.keys(c);
-    for (let i=0; i<_n.length; i++) {
-        let _x = _n[i].x;
-        let _y = _n[i].y;
-        let _w = arr[_y][_x]; 
-        if (k.includes(_w)) {
-            let _m = neigh({ x: _n[i].x, y: _n[i].y})
-            rs(_m, c[_w]);
-        }
-    }
-}
+let _final = [];
 
 const word = [
     "this",
@@ -35,7 +23,7 @@ const word = [
     "boggle",
     "board",
     "test",
-    "REPEAT",
+    "REPEATED",
     "NOTRE-PEATED"
 ];
 
@@ -43,13 +31,14 @@ function td () {
     let tree = {};
     word.forEach(ele => {
         const wd = ele.split("");
-        if (!tree[wd[0]]) {
-            tree[wd[0]] = {};
-        }
-        let p = tree[wd[0]];
-        for (let i = 1; i < wd.length; i++) {
+        let p = tree;
+        for (let i = 0; i < wd.length; i++) {
             if (p[wd[i]] === undefined) {
-                p[wd[i]] = {}
+                if (i === wd.length-1) {
+                    p[wd[i]] = ele;
+                } else {
+                    p[wd[i]] = {}
+                }
             }
             p = p[wd[i]];
         }
@@ -57,21 +46,26 @@ function td () {
     return tree;
 }
 const tree = td();
-
 function tt () {
     arr.forEach((ele, y) => {
-        const wd = ele.split('');
+        const wd = ele;
         for (let x =0; x<wd.length; x++) { 
             vt = [];
             let _l = wd[x];
             if (tree[_l]) {
                 vt.push(`${x}${y}`);
                 let c = tree[_l];
-               const _n = neigh({ x, y });
-               
+                const _n = neigh({ x, y });
+                let _r = rs(_n, c);
+                if (_r) {
+                    if (!_final.includes(_r)) {
+                        _final.push(_r);
+                    }
+                }
             }
         }
-    })
+    });
+    return _final;
 }
 
 function neigh ({ x, y}) {
@@ -85,31 +79,78 @@ function neigh ({ x, y}) {
     } 
     if (y>0 && x < w) {
         let ur = { x: x+1, y: y-1 };
-        _n.push(ur);
+        let _s = `${x+1}${y-1}`;
+        if (!vt.includes(_s)) {
+            _n.push(ur);
+        }
     } 
     if (x < w) {
         let r = { x: x+1, y };
-        _n.push(r);
+        let _s = `${x+1}${y}`;
+        if (!vt.includes(_s)) {
+            _n.push(r);
+        }
     } 
     if (y < h && x < w) {
         let dr = { x: x+1, y: y+1 };
-        _n.push(dr);
+        let _s = `${x+1}${y+1}`;
+        if (!vt.includes(_s)) {
+            _n.push(dr);
+        }
     } 
     if (y < h) {
         let d = { x, y: y+1 };
-        _n.push(d);
+        let _s = `${x}${y+1}`;
+        if (!vt.includes(_s)) {
+            _n.push(d);
+        }
     } 
     if (y < h && x > 0) {
         let dl = { x: x-1, y: y+1 };
-        _n.push(dl);
+        let _s = `${x-1}${y+1}`;
+        if (!vt.includes(_s)) {
+            _n.push(dl);
+        }
     } 
     if (x > 0) {
         let l = { x: x-1, y };
-        _n.push(l);
+        let _s = `${x-1}${y}`;
+        if (!vt.includes(_s)) {
+            _n.push(l);
+        }
     } 
     if (y > 0 && x > 0) {
         let ul = { x: x-1, y: y-1 };
-        _n.push(ul);
+        let _s = `${x-1}${y-1}`;
+        if (!vt.includes(_s)) {
+            _n.push(ul);
+        }
     } 
     return _n;
 }
+
+function rs (_n, c) {
+    if (typeof(c) !== "object") {
+        console.log(c)
+        return c;
+    }
+    let k = Object.keys(c);
+    for (let i=0; i<_n.length; i++) {
+        let _x = _n[i].x;
+        let _y = _n[i].y;
+        let _w = arr[_y][_x];
+        if (k.includes(_w)) {
+            vt.push(`${_x}${_y}`);
+            let _m = neigh({ x: _n[i].x, y: _n[i].y})
+            const _r  = rs(_m, c[_w]);
+            if (!_r) {
+                vt.splice(vt.indexOf(`${_x}${_y}`), 1);
+            } else {
+                return _r;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(tt());
