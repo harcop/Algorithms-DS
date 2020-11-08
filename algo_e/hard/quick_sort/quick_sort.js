7e0edd94153617857979e92f65b2f64adab7addc
harcop <harcopdrew@gmail.com>
2020-11-08 19:19:46 +0100

    fix(): fix quick select bugs

--- algo_e/hard/quick_sort/quick_sort.js
+++ algo_e/hard/quick_sort/quick_sort.js
@@ -1,11 +1,10 @@
-const arr = [8,5,2,9,5,6,3];
+// const arr = [8,5,2,9,5,6,3];
 
 //time o(log(n)) and space O(log(n))
 function qs(arr, low, high) {
     const p = partition(arr, low, high);
     if (p > low) {
         qs(arr,0,p)
-        console.log(arr)
         qs(arr,p+1,high)
     }
     return arr;
@@ -13,23 +12,22 @@ function qs(arr, low, high) {
 
 function partition(arr, low, high) {
     let p = low;
-    console.log(low, high);
-    console.log(arr.slice(low, high))
     for(let i = low+1; i<high; i++) {
         if (arr[i] < arr[p]) {
-            swap(i, p);
-            console.log(p)
+            swap(i, p, arr);
             p = i;
         } else {
             p = i;
         }
     }
-    console.log(p)
     return p;
 }
 
-function swap(a,b) {
+function swap(a,b,arr) {
     [arr[a],arr[b]] = [arr[b],arr[a]];
 }
 
-console.log(qs(arr, 0, arr.length))
+// console.log(qs(arr, 0, arr.length));
+// const r = qs(arr, 0, arr.length);
+// console.log(r);
+module.exports.qs = qs;
