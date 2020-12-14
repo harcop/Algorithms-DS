8856160d780012407a09133fd8877928081c62dd
harcop <harcopdrew@gmail.com>
2020-12-14 21:59:23 +0100

    feat(): mini interpreter

--- Input.java
+++ Input.java
@@ -9,9 +9,9 @@ public class Input {
         Scanner in = new Scanner(System.in);
         String username = in.nextLine();
         // String username = in.read();
-        System.out.println(username.substring(0,1));
-        System.out.println(username.substring(0,5));
-        System.out.println(username.substring(3,5));
+        // System.out.println(username.substring(0,1));
+        // System.out.println(username.substring(0,5));
+        // System.out.println(username.substring(3,5));
 
     }
 }
