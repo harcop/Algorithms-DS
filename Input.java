c999e97fce1f8e1c014af96e6d8c1338694f056b
Toluwap <33985313+harcop@users.noreply.github.com>
2021-01-05 23:22:13 +0100

    Update Input.java

--- Input.java
+++ Input.java
@@ -8,10 +8,5 @@ public class Input {
         // InputStreamReader in = new InputStreamReader(System.in);
         Scanner in = new Scanner(System.in);
         String username = in.nextLine();
-        // String username = in.read();
-        // System.out.println(username.substring(0,1));
-        // System.out.println(username.substring(0,5));
-        // System.out.println(username.substring(3,5));
-
     }
 }
