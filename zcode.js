1d6d64643ace2a740aa8796900f035b79399b824
harcop <harcopdrew@gmail.com>
2021-01-16 05:30:49 +0100

    feat(): transpiler

--- zcode.js
+++ zcode.js
@@ -1,4 +1,22 @@
-function namer() { 
-    console.logs('endsar'); 
+const fs = require('fs');
+const { transpiler } = require('./transpiler');
+const { exec } = require('child_process');
+
+let code = `function namer() { 
+    sorosoke.werey('endsar'); 
 } 
-namer();
+namer();`;
+code = transpiler(code);
+fs.writeFileSync("./zcode.js", code);
+
+exec("node zcode", (error, stderr, stdout) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
