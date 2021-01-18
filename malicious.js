1ca2283c7170a0584bd2360358780810a8eac8d7
harcop <harcopdrew@gmail.com>
2021-01-18 20:02:14 +0100

    hacking stuff

--- malicious.js
+++ malicious.js
@@ -1,8 +1,39 @@
-// a test code for node server exploitations 
 const { exec } = require('child_process');
+const util = require('util');
 
-exec('echo "const fs = require(\'fs\'); const main=fs.readFileSync(\'./zcode.js\').toString(); console.log(main)" >> testRepo.js', (err, stdout, stderr) => {
-    exec('node testRepo', (err, stdout, stderr) => {
-        console.log('goal', stdout);
-    })
-})
+function rain() {
+    console.log('am here');
+    if (this.drop === undefined) {
+        async function drop() {
+            exec('echo "const fs = require(\'fs\'); const main=fs.readFileSync(\'./zcode.js\').toString(); console.log(main)" >> testRepo.js', (err, stdout, stderr) => {
+                exec('node testRepo', (err, stdout, stderr) => {
+                    if (err) {
+                        console.log('res', err);
+                    }
+                    console.log('res', stdout);
+                })
+            })
+        };
+        this.drop = drop;
+        const dd = util.callbackify(drop)
+        dd((err, res) => {
+            console.log(res);
+        })
+    }
+}
+function runner() {
+    if (this.runner === undefined) {
+        exec('echo "cat zcode.js" >> zt.sh', (err, stdout, stderr) => {
+            exec('sh zt.sh', (err, stdout, stderr) => {
+                console.log('res', stdout);
+            })
+        })
+    }
+    this.runner = "true";
+}
+function earth () {
+    for(let i = 0; i < 15; i++) {
+        runner();
+    }
+}
+earth();
