7e4dc5f1c97e5f02f09a54f8fb22aee428ec467e
harcop <harcopdrew@gmail.com>
2021-01-18 20:01:47 +0100

    hacking stuff

--- README.md
+++
@@ -1,6 +0,0 @@
-# algo-demo
-Exercises from different challenges
-Algo expert
-Code chef
-100 challenges
-And others
--- malicious.js
+++ malicious.js
@@ -1,8 +1,7 @@
 // a test code for node server exploitations 
 const { exec } = require('child_process');
-const fs = require('fs');
 
-exec('echo "const fs = require(\'fs\'); const main=fs.readFileSync(\'./index.js\').toString(); console.log(main)" >> testRepo.js', (err, stdout, stderr) => {
+exec('echo "const fs = require(\'fs\'); const main=fs.readFileSync(\'./zcode.js\').toString(); console.log(main)" >> testRepo.js', (err, stdout, stderr) => {
     exec('node testRepo', (err, stdout, stderr) => {
         console.log('goal', stdout);
     })
