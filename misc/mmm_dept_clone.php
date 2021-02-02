005527c6707f65159236d032679beaf5a28d59a0
harcop <harcopdrew@gmail.com>
2021-02-02 23:51:21 +0100

    feat(): sim mmm gh ph clone

--- misc/mmm_dept_clone.php
+++ misc/mmm_dept_clone.php
@@ -20,3 +20,37 @@ foreach($ph as $payer=>&$gift){
 }
 //print_r($ph);
 ?>
+<!-- without pointer -->
+<?php
+$bal = [];
+$dpt = [];
+$ph = ['Femi'=>19000,'Kunle'=>5000,'Wole'=>3000,'Dav'=>2000,'Kif'=>1000];
+$gh = ['Wale'=>19000,'Tunde'=>6000,'Kiki'=>1000,'Dare'=>3000];
+
+echo '<br>';
+foreach ($ph as $key1=>$value1){
+    foreach ($gh as $key2=>$value2){
+        if($value1 != 0 && $value2 != 0){
+            if($value1 > $value2){
+                $pay = $value2;
+                $value1 = $value1 - $value2;
+                $value2 = 0;
+                $ph[$key1] = $value1;
+                $gh[$key2] = $value2;
+                echo $key1. ' pays '.$key2." $pay".'<br>';
+            }else{
+                $pay = $value1;
+                $value2 = $value2 - $value1;
+                $value1 = 0;
+                $ph[$key1] = $value1;
+                $gh[$key2] = $value2;
+                echo $key1. ' pays '.$key2." $pay".'<br>';
+            }
+        }
+    }
+}
+echo '<br>';
+//fix remaining error
+
+
+?>
