dab472cc5538a34fef3f5bb49558aa8bb34183bb
harcop <harcopdrew@gmail.com>
2021-02-12 05:30:38 +0100

    feat(): add selection sort

---
+++ sort/2 selection.js
@@ -0,0 +1,24 @@
+// 54386791
+const arr = [5,4,3,8,6,7,9,1];
+function selection () {
+    let i = 0;
+    while(i < arr.length) {
+        let min = arr[i];
+        let _m = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                _m = j
+            }
+        }
+        swap(i, _m);
+        i++;
+    }
+    return arr;
+}
+
+function swap(a, b) {
+    [arr[a], arr[b]] = [arr[b], arr[a]]; 
+}
+
+console.log(selection());
