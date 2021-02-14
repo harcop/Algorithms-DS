19cbf2e2bc7946008f15922ee57c997dbf6e7e89
harcop <harcopdrew@gmail.com>
2021-02-14 22:33:35 +0100

    feat(): add insertion sort

--- sort/2_selection.js
+++ sort/2_selection.js
@@ -3,6 +3,7 @@ const arr = [5,4,3,8,6,7,9,1];
 
 function selection () {
     let i = 0;
+    //find the smallest among the elements and sort
     while(i < arr.length) {
         let min = arr[i];
         let _m = i;
