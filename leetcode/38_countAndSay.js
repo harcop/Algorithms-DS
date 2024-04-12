e8259fb75e27f3535909fe239f72232a45fc1a3e
harcop <harcopdrew@gmail.com>
2024-04-12 15:04:40 +0100

    38 countAndSay

---
+++ leetcode/38_countAndSay.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    return cns(n)
+};
+
+function cns(n) {
+    if(n === 1) {
+        return 1
+    }
+    let nn = 1;
+    let currentSay = 1;
+    while(nn < n) {
+        currentSay = say(currentSay);
+        nn += 1;
+    }
+    return currentSay
+}
+
+function say(val) {
+  let vv = String(val)
+    const s = [];
+    let currentV = '';
+    let currentCount = 0;
+    for(let i = 0; i < vv.length; i++) {
+        let v = vv[i];
+        if(currentV === '') {
+            currentV = v
+        }
+        if(currentV === v) {
+            currentCount += 1;
+            if(vv[i + 1] !== v) {
+              s.push(`${currentCount}${currentV}`);
+              currentV = vv[i + 1];
+              currentCount = 0;
+            }
+        }
+    }
+    return s.join('')
+}
+
+
+console.log(countAndSay(1))
+console.log(countAndSay(2))
+console.log(countAndSay(3))
+console.log(countAndSay(4))
+console.log(countAndSay(5))
