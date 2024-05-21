89a8952bf5da19c55b48c65b60178472d825a034
harcop <harcopdrew@gmail.com>
2024-05-21 12:56:08 +0100

    Add performance check

--- duplicate.js
+++ duplicate.js
@@ -10,14 +10,22 @@ const getDuplicatesFromArray = function(array){
     return duplicates;
 };
 
+const start = performance.now();
+console.log(getDuplicatesFromArray([1,1,3,5,6,3,7]))
+const end = performance.now();
+const duration = end - start;
+console.log(`${duration} milliseconds.`);
+
 
 const returnDuplicate = (array) => {
   const duplicates = [];
   const seens = {};
-  array.forEach((item) => seens[item] ? duplicates.push(item) : seens[item] = item)
+  array.forEach((item) => seens[item] ? duplicates.push(item) : seens[item] = true)
   return duplicates
 }
 
-console.log(getDuplicatesFromArray([1,1,3,5,6,3,7]))
+const start2 = performance.now();
 console.log(returnDuplicate([1,1,3,5,6,3,7]))
-
+const end2 = performance.now();
+const duration2 = end2 - start2;
+console.log(`${duration2} milliseconds.`);
