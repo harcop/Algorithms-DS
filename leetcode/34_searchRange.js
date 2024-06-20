7a71e1de09b4153eb332e088a0374d83eb6e3d70
harcop <harcopdrew@gmail.com>
2024-06-20 22:45:58 +0100

    add problem description

--- leetcode/34_searchRange.js
+++ leetcode/34_searchRange.js
@@ -1,3 +1,9 @@
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
 const searchRange = function(nums, target) {
     let left = -1;
     let right = -1;
