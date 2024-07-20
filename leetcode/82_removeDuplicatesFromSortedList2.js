36207b86d3bef13871d05cde05708c340ac319a3
harcop <harcopdrew@gmail.com>
2024-07-21 00:31:13 +0100

    82 remove duplicate from sorted list 2

--- leetcode/82_removeDuplicatesFromSortedList2.js
+++ leetcode/82_removeDuplicatesFromSortedList2.js
@@ -10,26 +10,34 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    let tempHead = head;
     let current = head;
-    let seen = [];
-    let previous = null
-    let previous2 = null
+    let seen = []
     while(current !== null) {
         let currentValue = current.val;
+        let currentNext = current.next;
         let i = 0;
-        let currentNext = current.next
-        if(currentNext) {
-            while(currentNext.val === currentValue) {
-                
-                i++
+        while(currentNext && currentNext.val === currentValue) {
+            currentNext = currentNext.next;
+            current.next = currentNext;
+            i++
+        }
+        if(i > 0) {
+            seen.push(current.val)
+        }
+        current = current.next
+    }
+    current = head;
+    while(current !== null) {
+        if(seen.includes(head.val)) {
+            head = head.next
+            current = head;
+        } else {
+            if(current.next && seen.includes(current.next.val)) {
+                current.next = current.next.next
+            } else {
+                current = current.next
             }
-
         }
     }
+    return head
 };
-
-function ListNode(val, next) {
-  this.val = (val===undefined ? 0 : val)
-  this.next = (next===undefined ? null : next)
-}
