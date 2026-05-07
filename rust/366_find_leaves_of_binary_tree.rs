37fa55d3a9944f8e763167027a912286e8454e98
harcop <harcopdrew@gmail.com>
2026-05-07 23:49:42 +0100

    find leaves of binary tree

--- rust/366_find_leaves_of_binary_tree.rs
+++ rust/366_find_leaves_of_binary_tree.rs
@@ -1,16 +1,56 @@
-/// LeetCode #366 - Find Leaves Of Binary Tree (premium; reference only)
-pub const NOTE: &str = "Implementation omitted in this set.";
+/// LeetCode #366 - Find Leaves of Binary Tree
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Box<TreeNode>>,
+    pub right: Option<Box<TreeNode>>,
+}
+
+fn find_leaves(mut root: Option<Box<TreeNode>>) -> Vec<Vec<i32>> {
+    let mut ans = vec![];
+    fn dfs(o: &mut Option<Box<TreeNode>>, ans: &mut Vec<Vec<i32>>) -> isize {
+        match o.as_mut() {
+            None => -1,
+            Some(n) => {
+                let l = dfs(&mut n.left, ans);
+                let r = dfs(&mut n.right, ans);
+                let h = l.max(r) + 1;
+                let hi = h as usize;
+                if ans.len() <= hi {
+                    ans.resize_with(hi + 1, Vec::new);
+                }
+                ans[hi].push(n.val);
+                h
+            }
+        }
+    }
+    dfs(&mut root, &mut ans);
+    ans
+}
 
 fn main() {
-    println!("{}", NOTE.len());
+    println!("{}", find_leaves(None).len());
 }
 
 #[cfg(test)]
 mod tests {
-    use super::NOTE;
+    use super::*;
 
     #[test]
-    fn note_non_empty() {
-        assert!(!NOTE.is_empty());
+    fn ex() {
+        let root = Some(Box::new(TreeNode {
+            val: 1,
+            left: Some(Box::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None,
+            })),
+            right: Some(Box::new(TreeNode {
+                val: 3,
+                left: None,
+                right: None,
+            })),
+        }));
+        assert_eq!(find_leaves(root), vec![vec![2, 3], vec![1]]);
     }
 }
