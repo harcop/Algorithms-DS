898c29a0a7347c79b2da654cc7db074ec1dca92f
harcop <harcopdrew@gmail.com>
2026-05-07 23:49:40 +0100

    sort characters by frequency

---
+++ rust/451_sort_characters_by_frequency.rs
@@ -0,0 +1,32 @@
+/// LeetCode #451 - Sort Characters By Frequency
+use std::collections::HashMap;
+
+fn frequency_sort(s: String) -> String {
+    let mut m = HashMap::new();
+    for ch in s.chars() {
+        *m.entry(ch).or_insert(0usize) += 1;
+    }
+    let mut v: Vec<(char, usize)> = m.into_iter().collect();
+    v.sort_by(|a, b| b.1.cmp(&a.1).then_with(|| a.0.cmp(&b.0)));
+    let mut out = String::new();
+    for (ch, c) in v {
+        for _ in 0..c {
+            out.push(ch);
+        }
+    }
+    out
+}
+
+fn main() {
+    println!("{}", frequency_sort("tree".into()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::frequency_sort;
+
+    #[test]
+    fn example_one() {
+        assert_eq!(frequency_sort("tree".into()), "eert");
+    }
+}
