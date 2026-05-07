c9d20d8454e5d50709e61a14b7f3082a08dab31e
harcop <harcopdrew@gmail.com>
2026-05-07 23:49:41 +0100

    hamming distance

---
+++ rust/461_hamming_distance.rs
@@ -0,0 +1,24 @@
+/// LeetCode #461 - Hamming Distance
+fn hamming_distance(x: i32, y: i32) -> i32 {
+    let mut z = (x as u32) ^ (y as u32);
+    let mut c = 0;
+    while z != 0 {
+        c += (z & 1) as i32;
+        z >>= 1;
+    }
+    c
+}
+
+fn main() {
+    println!("{}", hamming_distance(1, 4));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::hamming_distance;
+
+    #[test]
+    fn example_one() {
+        assert_eq!(hamming_distance(1, 4), 2);
+    }
+}
