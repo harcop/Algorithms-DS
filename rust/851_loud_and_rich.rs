0bcab7bdd414775f957e3354762bc78874b10138
harcop <harcopdrew@gmail.com>
2026-05-16 10:51:59 +0100

    loud and rich

--- rust/851_loud_and_rich.rs
+++ rust/851_loud_and_rich.rs
@@ -2,28 +2,32 @@
 fn loud_and_rich(richer: Vec<Vec<i32>>, quiet: Vec<i32>) -> Vec<i32> {
     let n = quiet.len();
     let mut g = vec![vec![]; n];
+    let mut indeg = vec![0; n];
     for e in richer {
         let a = e[0] as usize;
         let b = e[1] as usize;
         g[b].push(a);
+        indeg[a] += 1;
     }
-    let mut memo = vec![-1i32; n];
-
-    fn dfs(u: usize, g: &[Vec<usize>], quiet: &[i32], memo: &mut [i32]) -> i32 {
-        if memo[u] != -1 {
-            return memo[u];
+    let mut ans: Vec<usize> = (0..n).collect();
+    let mut q = std::collections::VecDeque::new();
+    for i in 0..n {
+        if indeg[i] == 0 {
+            q.push_back(i);
         }
-        memo[u] = u as i32;
+    }
+    while let Some(u) = q.pop_front() {
         for &v in &g[u] {
-            let cand = dfs(v, g, quiet, memo);
-            if quiet[cand as usize] < quiet[memo[u] as usize] {
-                memo[u] = cand;
+            if quiet[ans[v]] > quiet[ans[u]] {
+                ans[v] = ans[u];
+            }
+            indeg[v] -= 1;
+            if indeg[v] == 0 {
+                q.push_back(v);
             }
         }
-        memo[u]
     }
-
-    (0..n).map(|i| dfs(i, &g, &quiet, &mut memo)).collect()
+    ans.into_iter().map(|x| x as i32).collect()
 }
 
 fn main() {
@@ -41,7 +45,7 @@ mod tests {
     fn example_one() {
         assert_eq!(
             loud_and_rich(vec![vec![1, 0], vec![2, 1]], vec![1, 0, 2]),
-            vec![1, 0, 1]
+            vec![0, 1, 1]
         );
     }
 }
