fcb8aaf20bc6ddcddbb1f78f4b90695b8287935b
harcop <harcopdrew@gmail.com>
2026-05-16 09:26:35 +0100

    lemonade change

---
+++ rust/860_lemonade_change.rs
@@ -0,0 +1,48 @@
+/// LeetCode #860 - Lemonade Change
+fn lemonade_change(bills: Vec<i32>) -> bool {
+    let mut five = 0;
+    let mut ten = 0;
+    for b in bills {
+        match b {
+            5 => five += 1,
+            10 => {
+                if five == 0 {
+                    return false;
+                }
+                five -= 1;
+                ten += 1;
+            }
+            20 => {
+                if ten > 0 && five > 0 {
+                    ten -= 1;
+                    five -= 1;
+                } else if five >= 3 {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+            _ => return false,
+        }
+    }
+    true
+}
+
+fn main() {
+    println!("{}", lemonade_change(vec![5, 5, 5, 10, 20]));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::lemonade_change;
+
+    #[test]
+    fn example_one() {
+        assert!(lemonade_change(vec![5, 5, 5, 10, 20]));
+    }
+
+    #[test]
+    fn example_two() {
+        assert!(!lemonade_change(vec![5, 5, 10, 10, 20]));
+    }
+}
