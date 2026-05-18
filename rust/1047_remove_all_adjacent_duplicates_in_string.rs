5c9d8aa166ca55ab13a26920cb9eab0a49842c17
harcop <harcopdrew@gmail.com>
2026-05-18 16:31:16 +0100

    remove all adjacent duplicates in string

---
+++ rust/1047_remove_all_adjacent_duplicates_in_string.rs
@@ -0,0 +1,31 @@
+/// LeetCode #1047 - Remove All Adjacent Duplicates In String
+fn remove_duplicates(s: String) -> String {
+    let mut st: Vec<char> = Vec::new();
+    for c in s.chars() {
+        if st.last() == Some(&c) {
+            st.pop();
+        } else {
+            st.push(c);
+        }
+    }
+    st.into_iter().collect()
+}
+
+fn main() {
+    println!("{}", remove_duplicates("abbaca".into()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::remove_duplicates;
+
+    #[test]
+    fn example_one() {
+        assert_eq!(remove_duplicates("abbaca".into()), "ca");
+    }
+
+    #[test]
+    fn example_two() {
+        assert_eq!(remove_duplicates("azxxzy".into()), "ay");
+    }
+}
