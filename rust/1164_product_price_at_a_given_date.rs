570bc42f8b80c3ffd26427e949c428cc7a64b58e
harcop <harcopdrew@gmail.com>
2026-05-20 07:42:03 +0100

    product price at a given date

---
+++ rust/1164_product_price_at_a_given_date.rs
@@ -0,0 +1,16 @@
+/// LeetCode #1164 - Product Price at a Given Date
+pub const NOTE: &str = "SQL problem; omitted in this set.";
+
+fn main() {
+    println!("{}", NOTE.len());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NOTE;
+
+    #[test]
+    fn note_non_empty() {
+        assert!(!NOTE.is_empty());
+    }
+}
