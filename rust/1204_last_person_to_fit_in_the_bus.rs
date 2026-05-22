98e40afe154c3669599e66bab6d719cbf755e4f4
harcop <harcopdrew@gmail.com>
2026-05-22 08:44:49 +0100

    last person to fit in the bus

---
+++ rust/1204_last_person_to_fit_in_the_bus.rs
@@ -0,0 +1,16 @@
+/// LeetCode #1204 - Last Person to Fit in the Bus
+pub const NOTE: &str = "SQL problem; omitted in this set.";
+
+fn main() {
+    println!("{}", NOTE.len());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NOTE;
+
+    #[test]
+    fn note_non_empty() {
+        assert!(!NOTE.is_empty());
+    }
+}
