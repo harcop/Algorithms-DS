9d07e76b9172252d6ff8df008402bf2c46a52a72
harcop <harcopdrew@gmail.com>
2026-05-22 08:44:49 +0100

    queries quality and percentage

---
+++ rust/1211_queries_quality_and_percentage.rs
@@ -0,0 +1,16 @@
+/// LeetCode #1211 - Queries Quality and Percentage
+pub const NOTE: &str = "SQL problem; omitted in this set.";
+
+fn main() {
+    println!("{}", NOTE.len());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NOTE;
+
+    #[test]
+    fn note_non_empty() {
+        assert!(!NOTE.is_empty());
+    }
+}
