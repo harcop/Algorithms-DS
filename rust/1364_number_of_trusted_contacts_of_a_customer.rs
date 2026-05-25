e3bbe99d35c94646cb9954bc26ccb5a8cec86518
harcop <harcopdrew@gmail.com>
2026-05-25 07:25:17 +0100

    number of trusted contacts of a customer

---
+++ rust/1364_number_of_trusted_contacts_of_a_customer.rs
@@ -0,0 +1,5 @@
+/// LeetCode #1364 - Number Of Trusted Contacts Of A Customer
+pub const NOTE: &str = "SQL problem; omitted in this set.";
+fn main() { println!("{}", NOTE.len()); }
+#[cfg(test)]
+mod tests { use super::NOTE; #[test] fn note_non_empty() { assert!(!NOTE.is_empty()); } }
