79d3c567dd6fd38406d599615c3c671b228d16cb
harcop <harcopdrew@gmail.com>
2026-05-27 06:33:21 +0100

    delete n nodes after m nodes of a linked list

---
+++ rust/1474_delete_n_nodes_after_m_nodes_of_a_linked_list.rs
@@ -0,0 +1,5 @@
+/// LeetCode #1474 - Delete N Nodes After M Nodes Of A Linked List
+pub const NOTE: &str = "SQL problem; omitted in this set.";
+fn main() { println!("{}", NOTE.len()); }
+#[cfg(test)]
+mod tests { use super::NOTE; #[test] fn note_non_empty() { assert!(!NOTE.is_empty()); } }
