d75edf654c556255f4eb6ed2c4313533e2968197
harcop <harcopdrew@gmail.com>
2026-06-01 22:27:46 +0100

    number of calls between two persons

---
+++ rust/1699_number_of_calls_between_two_persons.rs
@@ -0,0 +1,5 @@
+/// LeetCode #1699 - Number Of Calls Between Two Persons
+pub const NOTE: &str = "SQL problem; omitted in this set.";
+fn main() { println!("{}", NOTE.len()); }
+#[cfg(test)]
+mod tests { use super::NOTE; #[test] fn note_non_empty() { assert!(!NOTE.is_empty()); } }
