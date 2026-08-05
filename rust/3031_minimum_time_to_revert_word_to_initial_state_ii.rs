7410c8ea46d1e04499c876745d4ae5567096f859
harcop <harcopdrew@gmail.com>
2026-08-05 11:47:48 +0100

    minimum time to revert word to initial state ii

---
+++ rust/3031_minimum_time_to_revert_word_to_initial_state_ii.rs
@@ -0,0 +1,37 @@
+/// LeetCode #3031 - Minimum Time to Revert Word to Initial State II
+fn minimum_time_to_revert(word: String, k: i32) -> i32 {
+    let k = k as usize;
+    let n = word.len();
+    let bytes = word.as_bytes();
+
+    for i in (k..n).step_by(k) {
+        if &bytes[i..] == &bytes[..n - i] {
+            return (i / k) as i32;
+        }
+    }
+    ((n + k - 1) / k) as i32
+}
+
+fn main() {
+    println!("{}", minimum_time_to_revert("abacaba".into(), 3));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::minimum_time_to_revert;
+
+    #[test]
+    fn example1() {
+        assert_eq!(minimum_time_to_revert("abacaba".into(), 3), 2);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(minimum_time_to_revert("abacaba".into(), 4), 1);
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(minimum_time_to_revert("abcbabcd".into(), 2), 4);
+    }
+}
