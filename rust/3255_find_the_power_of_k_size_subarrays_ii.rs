9bd404749c0b691d93443da08b4fb5301da55e53
harcop <harcopdrew@gmail.com>
2026-08-18 09:02:52 +0100

    find the power of k-size subarrays ii

---
+++ rust/3255_find_the_power_of_k_size_subarrays_ii.rs
@@ -0,0 +1,44 @@
+/// LeetCode #3255 - Find the Power of K-Size Subarrays II
+fn results_array(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let n = nums.len();
+    let k = k as usize;
+    let mut f = vec![1; n];
+    for i in 1..n {
+        if nums[i] == nums[i - 1] + 1 {
+            f[i] = f[i - 1] + 1;
+        }
+    }
+    (k - 1..n)
+        .map(|i| if f[i] >= k { nums[i] } else { -1 })
+        .collect()
+}
+
+fn main() {
+    println!("{:?}", results_array(vec![1, 2, 3, 4, 3, 2, 5], 3));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::results_array;
+
+    #[test]
+    fn example1() {
+        assert_eq!(
+            results_array(vec![1, 2, 3, 4, 3, 2, 5], 3),
+            vec![3, 4, -1, -1, -1]
+        );
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(results_array(vec![2, 2, 2, 2, 2], 4), vec![-1, -1]);
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(
+            results_array(vec![3, 2, 3, 2, 3, 2], 2),
+            vec![-1, 3, -1, 3, -1]
+        );
+    }
+}
