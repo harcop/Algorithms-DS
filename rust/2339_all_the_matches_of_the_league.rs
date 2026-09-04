6fb6ed82907ab8f744d8dfcf40fd4a1849defe1f
harcop <harcopdrew@gmail.com>
2026-09-04 08:15:44 +0100

    all the matches of the league

--- rust/2339_all_the_matches_of_the_league.rs
+++ rust/2339_all_the_matches_of_the_league.rs
@@ -1,16 +1,48 @@
-/// LeetCode #2339 - All the Matches of the League
-pub const NOTE: &str = "SQL problem; omitted in this set.";
+/// LeetCode #2339 - All the Matches of the League (SQL; Rust analogue)
+fn all_league_matches(teams: Vec<String>) -> Vec<(String, String)> {
+    let mut ans = Vec::new();
+    for home in &teams {
+        for away in &teams {
+            if home != away {
+                ans.push((home.clone(), away.clone()));
+            }
+        }
+    }
+    ans.sort();
+    ans
+}
 
 fn main() {
-    println!("{}", NOTE.len());
+    println!(
+        "{:?}",
+        all_league_matches(vec![
+            "Leetcode FC".into(),
+            "Ahly SC".into(),
+            "Real Madrid".into()
+        ])
+    );
 }
 
 #[cfg(test)]
 mod tests {
-    use super::NOTE;
+    use super::all_league_matches;
 
     #[test]
-    fn note_non_empty() {
-        assert!(!NOTE.is_empty());
+    fn example_one() {
+        let teams = vec![
+            "Leetcode FC".into(),
+            "Ahly SC".into(),
+            "Real Madrid".into(),
+        ];
+        let mut expected = vec![
+            ("Real Madrid".into(), "Leetcode FC".into()),
+            ("Real Madrid".into(), "Ahly SC".into()),
+            ("Leetcode FC".into(), "Real Madrid".into()),
+            ("Leetcode FC".into(), "Ahly SC".into()),
+            ("Ahly SC".into(), "Real Madrid".into()),
+            ("Ahly SC".into(), "Leetcode FC".into()),
+        ];
+        expected.sort();
+        assert_eq!(all_league_matches(teams), expected);
     }
 }
