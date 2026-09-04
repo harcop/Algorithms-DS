0623128aed8cf89645a236a5c437cb562ee4415a
harcop <harcopdrew@gmail.com>
2026-09-04 08:15:32 +0100

    non decreasing subsequences

--- rust/491_non_decreasing_subsequences.rs
+++ rust/491_non_decreasing_subsequences.rs
@@ -1,16 +1,62 @@
-/// LeetCode #491 - Non Decreasing Subsequences (placeholder)
-pub const NOTE: &str = "Solution placeholder; implement as needed.";
+/// LeetCode #491 - Non-decreasing Subsequences
+use std::collections::HashSet;
+
+fn find_subsequences(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut ans = Vec::new();
+    let mut path = Vec::new();
+    dfs(0, &nums, &mut path, &mut ans);
+    ans
+}
+
+fn dfs(start: usize, nums: &[i32], path: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>) {
+    if path.len() >= 2 {
+        ans.push(path.clone());
+    }
+    let mut used = HashSet::new();
+    for i in start..nums.len() {
+        if used.contains(&nums[i]) {
+            continue;
+        }
+        if path.last().map_or(true, |&x| nums[i] >= x) {
+            used.insert(nums[i]);
+            path.push(nums[i]);
+            dfs(i + 1, nums, path, ans);
+            path.pop();
+        }
+    }
+}
 
 fn main() {
-    println!("{}", NOTE.len());
+    println!("{:?}", find_subsequences(vec![4, 6, 7, 7]));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::NOTE;
+    use super::find_subsequences;
+    use std::collections::HashSet;
+
+    fn as_set(v: Vec<Vec<i32>>) -> HashSet<Vec<i32>> {
+        v.into_iter().collect()
+    }
 
     #[test]
-    fn note_non_empty() {
-        assert!(!NOTE.is_empty());
+    fn example_one() {
+        let got = as_set(find_subsequences(vec![4, 6, 7, 7]));
+        let want = as_set(vec![
+            vec![4, 6],
+            vec![4, 6, 7],
+            vec![4, 6, 7, 7],
+            vec![4, 7],
+            vec![4, 7, 7],
+            vec![6, 7],
+            vec![6, 7, 7],
+            vec![7, 7],
+        ]);
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn example_two() {
+        assert_eq!(find_subsequences(vec![4, 4, 3, 2, 1]), vec![vec![4, 4]]);
     }
 }
