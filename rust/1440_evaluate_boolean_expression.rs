83cdc25f752fdcb8ffe4602f57118deaf607db44
harcop <harcopdrew@gmail.com>
2026-09-04 08:15:41 +0100

    evaluate boolean expression

--- rust/1440_evaluate_boolean_expression.rs
+++ rust/1440_evaluate_boolean_expression.rs
@@ -1,5 +1,61 @@
-/// LeetCode #1440 - Evaluate Boolean Expression
-pub const NOTE: &str = "SQL problem; omitted in this set.";
-fn main() { println!("{}", NOTE.len()); }
+/// LeetCode #1440 - Evaluate Boolean Expression (SQL; Rust analogue)
+use std::collections::HashMap;
+
+fn evaluate_boolean(
+    variables: Vec<(String, i32)>,
+    expressions: Vec<(String, String, String)>,
+) -> Vec<(String, String, String, String)> {
+    let vals: HashMap<String, i32> = variables.into_iter().collect();
+    expressions
+        .into_iter()
+        .map(|(left, op, right)| {
+            let lv = vals[&left];
+            let rv = vals[&right];
+            let ok = match op.as_str() {
+                ">" => lv > rv,
+                "<" => lv < rv,
+                "=" => lv == rv,
+                _ => false,
+            };
+            (
+                left,
+                op,
+                right,
+                if ok { "true".into() } else { "false".into() },
+            )
+        })
+        .collect()
+}
+
+fn main() {
+    println!("{:?}", evaluate_boolean(vec![], vec![]));
+}
+
 #[cfg(test)]
-mod tests { use super::NOTE; #[test] fn note_non_empty() { assert!(!NOTE.is_empty()); } }
+mod tests {
+    use super::evaluate_boolean;
+
+    #[test]
+    fn example() {
+        let variables = vec![("x".into(), 66), ("y".into(), 77)];
+        let expressions = vec![
+            ("x".into(), ">".into(), "y".into()),
+            ("x".into(), "<".into(), "y".into()),
+            ("x".into(), "=".into(), "y".into()),
+            ("y".into(), ">".into(), "x".into()),
+            ("y".into(), "<".into(), "x".into()),
+            ("x".into(), "=".into(), "x".into()),
+        ];
+        assert_eq!(
+            evaluate_boolean(variables, expressions),
+            vec![
+                ("x".into(), ">".into(), "y".into(), "false".into()),
+                ("x".into(), "<".into(), "y".into(), "true".into()),
+                ("x".into(), "=".into(), "y".into(), "false".into()),
+                ("y".into(), ">".into(), "x".into(), "true".into()),
+                ("y".into(), "<".into(), "x".into(), "false".into()),
+                ("x".into(), "=".into(), "x".into(), "true".into()),
+            ]
+        );
+    }
+}
