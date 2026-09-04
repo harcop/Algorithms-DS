bc7e5104743625461e395552061a19ca836129b7
harcop <harcopdrew@gmail.com>
2026-09-04 08:15:33 +0100

    beautiful arrangement

--- rust/526_beautiful_arrangement.rs
+++ rust/526_beautiful_arrangement.rs
@@ -1,16 +1,39 @@
-/// LeetCode #526 - Beautiful Arrangement (placeholder)
-pub const NOTE: &str = "Solution placeholder; implement as needed.";
+/// LeetCode #526 - Beautiful Arrangement
+fn count_arrangement(n: i32) -> i32 {
+    let n = n as usize;
+    let mut used = vec![false; n + 1];
+    fn dfs(pos: usize, n: usize, used: &mut [bool]) -> i32 {
+        if pos > n {
+            return 1;
+        }
+        let mut cnt = 0;
+        for x in 1..=n {
+            if !used[x] && (x % pos == 0 || pos % x == 0) {
+                used[x] = true;
+                cnt += dfs(pos + 1, n, used);
+                used[x] = false;
+            }
+        }
+        cnt
+    }
+    dfs(1, n, &mut used)
+}
 
 fn main() {
-    println!("{}", NOTE.len());
+    println!("{}", count_arrangement(2));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::NOTE;
+    use super::count_arrangement;
 
     #[test]
-    fn note_non_empty() {
-        assert!(!NOTE.is_empty());
+    fn example_one() {
+        assert_eq!(count_arrangement(2), 2);
+    }
+
+    #[test]
+    fn example_two() {
+        assert_eq!(count_arrangement(1), 1);
     }
 }
