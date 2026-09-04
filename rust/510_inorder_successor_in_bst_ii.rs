d795ffa66ba86e71213c4da78199cf0774f66d5d
harcop <harcopdrew@gmail.com>
2026-09-04 08:15:33 +0100

    inorder successor in bst ii

--- rust/510_inorder_successor_in_bst_ii.rs
+++ rust/510_inorder_successor_in_bst_ii.rs
@@ -1,16 +1,63 @@
-/// LeetCode #510 - Inorder Successor In Bst Ii (premium; reference only)
-pub const NOTE: &str = "Implementation omitted in this set.";
+/// LeetCode #510 - Inorder Successor in BST II
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Box<TreeNode>>,
+    pub right: Option<Box<TreeNode>>,
+}
+
+impl TreeNode {
+    fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+fn inorder_successor(root: Option<&Box<TreeNode>>, p: i32) -> Option<i32> {
+    let mut succ = None;
+    let mut cur = root;
+    while let Some(n) = cur {
+        if n.val > p {
+            succ = Some(n.val);
+            cur = n.left.as_ref();
+        } else {
+            cur = n.right.as_ref();
+        }
+    }
+    succ
+}
 
 fn main() {
-    println!("{}", NOTE.len());
+    let mut root = Box::new(TreeNode::new(2));
+    root.left = Some(Box::new(TreeNode::new(1)));
+    root.right = Some(Box::new(TreeNode::new(3)));
+    println!("{:?}", inorder_successor(Some(&root), 1));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::NOTE;
+    use super::{inorder_successor, TreeNode};
 
     #[test]
-    fn note_non_empty() {
-        assert!(!NOTE.is_empty());
+    fn example_one() {
+        let mut root = Box::new(TreeNode::new(2));
+        root.left = Some(Box::new(TreeNode::new(1)));
+        root.right = Some(Box::new(TreeNode::new(3)));
+        assert_eq!(inorder_successor(Some(&root), 1), Some(2));
+    }
+
+    #[test]
+    fn example_two() {
+        let mut root = Box::new(TreeNode::new(5));
+        let mut l = Box::new(TreeNode::new(3));
+        l.left = Some(Box::new(TreeNode::new(2)));
+        l.right = Some(Box::new(TreeNode::new(4)));
+        l.left.as_mut().unwrap().left = Some(Box::new(TreeNode::new(1)));
+        root.left = Some(l);
+        root.right = Some(Box::new(TreeNode::new(6)));
+        assert_eq!(inorder_successor(Some(&root), 6), None);
     }
 }
